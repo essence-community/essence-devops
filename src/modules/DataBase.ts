6c1941c1f9f3b7197422f0d2bcf3d7ab1c786541
Ivan Artemov <honyrik@gmail.com>
2021-11-03 11:44:46 +0300

    Update TypeOrm Logger

--- src/modules/DataBase.ts
+++ src/modules/DataBase.ts
@@ -1,6 +1,7 @@
 
 import { TypeOrmModule } from '@nestjs/typeorm';
 import * as path from 'path';
+import { TypeOrmLogger } from '../TypeOrmLogger';
 
 export const DataBase = TypeOrmModule.forRoot({
     type: process.env.DB_TYPE as any,
@@ -12,6 +13,8 @@ export const DataBase = TypeOrmModule.forRoot({
     entities: [path.join(__dirname, '..', 'entities', '*{.ts,.js}'), path.join(__dirname, '..', 'entities', '**', '*{.ts,.js}')],
     migrations: [path.join(__dirname, '..', 'migrations', '*{.ts,.js}')],
     migrationsRun: process.env.DB_MIGRATIONSRUN === 'true',
-    logging: process.env.DB_LOGGING === 'true',
+    logging:  process.env.DB_LOGGING ? process.env.DB_LOGGING === 'true' : true,
+    logger: new TypeOrmLogger('DB'),
     synchronize: process.env.DB_SYNCHRONIZE === 'true',
+    extra: process.env.DB_EXTRA ? JSON.parse(process.env.DB_EXTRA) : undefined,
 });
